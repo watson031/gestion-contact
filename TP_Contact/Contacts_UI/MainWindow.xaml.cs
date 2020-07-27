71a61b2eef8e4e742d1b6764241c180e75acc4ab
Watson II Augustin <waugusti@isimtl.local>
2020-07-27 13:35:50 -0400

    MOdification des fichiers du projet

---
+++ TP_Contact/Contacts_UI/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Contacts_UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Click");
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Click");
+        }
+
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Click");
+        }
+    }
+}
